217c9f3808e39b3b028d7f96744af5cc204a3bb2
jstafford <CatMouse2017!>
2022-04-20 13:56:43 -0500

    Transferring balances in deploy_contracts.js. Added unit test to test deployment script.

--- defi-staking-app-starter/migrations/2_deploy_contracts.js
+++ defi-staking-app-starter/migrations/2_deploy_contracts.js
@@ -2,8 +2,19 @@ const Tether = artifacts.require('Tether');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(Tether);
+    const tether = await Tether.deployed();
+
     await deployer.deploy(RWD);
-    await deployer.deploy(DecentralBank);
+    const rwd = await RWD.deployed();
+
+    await deployer.deploy(DecentralBank, rwd.address, tether.address);
+    const decentralBank = await DecentralBank.deployed();
+
+    const allRwdTokens = '1000000000000000000000000';
+    await rwd.transfer(decentralBank.address, allRwdTokens);
+
+    const oneHundredMockTetherTokens = '100000000000000000000';
+    await tether.transfer(accounts[1], oneHundredMockTetherTokens)
 };
